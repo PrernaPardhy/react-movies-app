95ee489a776c17f389d11737be8e438b3970990c
PrernaPardhy <prerna.pardhy@gmailcom>
2019-01-29 14:27:20 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,6 +5,11 @@ import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
 
 const customStyles = {
     content:{
@@ -15,6 +20,14 @@ const customStyles = {
         marginRight: '-50%',
         transform:'translate(-50%,-50%)'
     }
+}
+
+const TabContainer = function(props){
+    return(
+        <Typography component="div" style={{padding:0}}>
+        {props.children}
+        </Typography>
+    )
 }
 
 class Header extends Component{
@@ -57,7 +70,18 @@ class Header extends Component{
                 <Tab label="Login"></Tab>
                 <Tab label="Register"></Tab>
                 </Tabs>
-
+                <TabContainer>
+                    <FormControl required>
+                        <InputLabel htmlFor="userName">UserName</InputLabel>
+                        <Input name="userName" type="text"></Input>
+                    </FormControl>
+                    <br />
+                    <FormControl>
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <Input name="password" type="password"></Input>
+                        
+                    </FormControl>
+                    </TabContainer>
 
 
                 </Modal>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './screens/home/Home'
 
+import 'typeface-roboto';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home /> , document.getElementById('root'));
